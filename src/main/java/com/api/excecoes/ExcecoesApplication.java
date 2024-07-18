6b27f86739d296678d482a8c41feed419321ca0a
gnoatx <85642178+gnoatx@users.noreply.github.com>
2024-07-17 23:16:43 -0300

    Exercícios 2 e 3 feitos

--- src/main/java/com/api/excecoes/ExcecoesApplication.java
+++ src/main/java/com/api/excecoes/ExcecoesApplication.java
@@ -3,13 +3,74 @@ package com.api.excecoes;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 @SpringBootApplication
 public class ExcecoesApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ExcecoesApplication.class, args);
+		clearConsole();
 
-		Exercicio.ex1();
+		Scanner scInt = new Scanner(System.in);
+		int input;
+		boolean continua = true;
+
+		while (continua) {
+			System.out.print(
+				"===== Menu =====\n" +
+				"Digite uma opção.\n" +
+				"1. ArithmeticException\n" +
+				"2. InputMismatchException\n" +
+				"3. ArrayIndexOutOfBoundsException\n" +
+				"4. Sair\n" +
+				"\n" +
+				"Opção: "
+			);
+	
+			try {
+				input = scInt.nextInt();
+			} catch (InputMismatchException e) {
+				System.out.println("Erro: Este campo apenas aceita números inteiros.");
+				continue;
+			}
+
+			switch (input) {
+				case 1:
+					Exercicio.ex1();
+					break;
+				case 2:
+					Exercicio.ex2();
+					break;
+				case 3:
+					Exercicio.ex3();
+					break;
+				case 4:
+					continua = false;
+					break;
+				default:
+					System.out.println("Opção inválida.");
+					break;
+			}
+		}
+
+		System.out.println("Saindo do programa.");
+		scInt.close();
 	}
 
+	static void clearConsole() {
+        try {
+            if (System.getProperty("os.name").contains("Windows")) {
+                new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();
+            } else if (System.getProperty("os.name").contains("Linux")
+                    || System.getProperty("os.name").contains("Mac")) {
+                new ProcessBuilder("clear").inheritIO().start().waitFor();
+            } else {
+                System.out.println("Este comando não funciona no seu sistema operacional.");
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+	}
 }
--- src/main/java/com/api/excecoes/Exercicio.java
+++ src/main/java/com/api/excecoes/Exercicio.java
@@ -1,5 +1,6 @@
 package com.api.excecoes;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Exercicio {
@@ -8,30 +9,91 @@ public class Exercicio {
     
 
     public static void ex1() {
-        double dividendo, divisor;
+        double dividendo, divisor, resultado;
 
         System.out.println("Digite o dividendo: ");
         dividendo = scDouble.nextDouble();
         System.out.println("Digite o divisor: ");
         divisor = scDouble.nextDouble();
+        resultado = dividendo / divisor;
 
         try {
-            System.out.println(dividendo + " / " + divisor + " = " + dividendo / divisor);
+            System.out.println(dividendo + " / " + divisor + " = " + resultado);
         } catch (ArithmeticException e) {
-            System.out.println("Erro: Impossível dividir por 0.");
+            System.out.println("Erro: Impossível dividir por 0."); //????
         }
     }
 
     public static void ex2() {
-        int input;
+        int input = 0;
+        boolean continua = true;
 
         System.out.print(
             "Escolha uma opção.\n" +
-            "1. \n" +
-            "2. \n" +
-            "3. \n" +
+            "1. Opção\n" +
+            "2. Outra opção diferente\n" +
+            "3. Opção mais diferente ainda\n" +
             "\n" +
             "Opção: "
         );
+
+        while (continua) {
+            try {
+                input = scInt.nextInt();
+                continua = false;
+            } catch (InputMismatchException e) {
+                System.out.print(
+                    "Erro: Este campo apenas aceita números inteiros.\n" +
+                    "Opção: "
+                );
+                scInt.next();
+            }
+        }
+
+        switch (input) {
+            case 1:
+                System.out.println("Uau... Opção 1...");
+                break;
+            case 2:
+                System.out.println("Nossa... Opção 2...");
+                break;
+            case 3:
+                System.out.println("Eita... Opção 3...");
+                break;
+            default:
+                System.out.println("Opção inválida.");
+                break;
+        }
+    }
+
+    public static void ex3() {
+        String[] nomes = {"Lucas", "Enzo", "Eloisa"};
+        int input = 0;
+        boolean continua = true;
+
+        System.out.println("Selecione um aluno.");
+        for (int i = 0; i < nomes.length; i++) {
+            System.out.println(i + 1 + ". " + nomes[i]);
+        }
+        System.out.print("\nOpção: ");
+
+        while (continua) {
+            try {
+                input = scInt.nextInt();
+                continua = false;
+            } catch (InputMismatchException e) {
+                System.out.print(
+                    "Erro: Este campo apenas aceita números inteiros.\n" +
+                    "Opção: "
+                );
+                scInt.next();
+            }
+        }
+
+        try {
+            System.out.println(nomes[input - 1] + " selecionado(a).");
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("Erro: Não existe um aluno neste índice.");
+        }
     }
 }
